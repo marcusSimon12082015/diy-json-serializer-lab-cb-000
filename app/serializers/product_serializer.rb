17f3dd7c21bda7a07fd68f31d83119ab6d5bdddd
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-05-25 16:15:54 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -5,7 +5,7 @@ class ProductSerializer
     serialized_product += '"id": ' + product.id.to_s + ', '
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"description": "' + product.description + '", '
-    serialized_product += '"inventory": "' + product.inventory.to_i + '"'
+    serialized_product += '"inventory": "' + product.inventory.to_s + '"'
     # and end with the close brace
     serialized_product += '}'
   end
